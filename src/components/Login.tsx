4270ad2501e9df41d2d2ff8596e4e2ee52284a10
Ivaylo <ivaylo.tsencov@gmail.com>
2024-01-26 16:06:07 +0200

    Create Login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import { v4 as uuidV4 } from 'uuid';
+import { Button, Container, Form } from "react-bootstrap";
+
+interface LoginProps {
+    onSubmitID: (id: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onSubmitID }): JSX.Element => {
+
+    const idRef = useRef<HTMLInputElement | null>(null);
+
+    const onSubmitLoginFormHandler = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        onSubmitID(idRef.current!.value);
+    }
+
+    const onClickCreateIDHandler = (): void => {
+        onSubmitID(uuidV4());
+    }
+
+    return(
+        <Container className="d-flex align-items-center justify-content-center" style={{height: '100vh'}}>
+            <Form onSubmit={ onSubmitLoginFormHandler } className="w-50">
+                <Form.Group className="mb-3">
+                    <Form.Label>Enter Your ID</Form.Label>
+                    <Form.Control ref={ idRef } type="text"/>
+                </Form.Group>
+                <Button type='submit' className="me-3">Login</Button>
+                <Button onClick={ onClickCreateIDHandler } variant='secondary'>Create new ID</Button>
+            </Form>
+        </Container>
+    )
+}
+
+export default Login;
